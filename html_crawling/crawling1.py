a358206cc82ac4381c592b74c8f6a361e5224159
hjso <hjsong9th@gmail.com>
2022-03-15 21:16:47 +0900

    base_code

--- html_crawling/crawling1.py
+++ html_crawling/crawling1.py
@@ -1,1 +1,18 @@
-print('plz plz I want freedom')
+from urllib import request
+import requests
+from bs4 import BeautifulSoup
+
+url='https://www.nbcnews.com/news/us-news/jamal-edwards-music-entrepreneur-helped-launch-ed-sheeran-jessie-j-die-rcna17041'
+
+response=requests.get(url)
+
+if response.status_code==200:
+    html=response.text
+    soup=BeautifulSoup(html, 'html.parser')
+    print(soup)
+    
+else:
+    print(response.status_code)
+    
+
+print('process done')
