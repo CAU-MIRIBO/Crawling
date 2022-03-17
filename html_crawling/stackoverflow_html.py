51d735eb48b7b76df46bc3235d464c3e2a8f717b
hjso <hjsong9th@gmail.com>
2022-03-17 22:10:10 +0900

    modify Beautifulsoup find-function tag keyword

--- html_crawling/stackoverflow_html.py
+++ html_crawling/stackoverflow_html.py
@@ -3,26 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 
 url='https://stackoverflow.com/questions/133698/why-does-fatal-error-lnk1104-cannot-open-file-c-program-obj-occur-when-i-c'
-
 def stackOverflow_process(soup):
     # print(soup)
     
-    ques_header=soup.find_all(attrs={'id': 'question-header'})
-    ques=ques_header[0].text
+    # get question header
+    ques_header=soup.find(attrs={'id': 'question-header'})
+    ques_header=ques_header.find('h1')
+    ques=ques_header.text
     
-    ques_contents=soup.find_all(attrs={'class':'s-prose js-post-body'})
-    ques_text=ques_contents=ques_contents[0].text
+    # get question contents
+    ques_content1=soup.find(attrs={'class':'question'})
+    ques_content2=ques_content1.find(attrs={'class':'s-prose js-post-body'})
+    ques_content=ques_content2.text
     
-    answer_child=soup.find_all(attrs={'class': 'js-accepted-answer-indicator flex--item fc-green-500 py6 mtn8'})[0]
-    answer_parent1=answer_child.parent.parent.parent
+    #get selected answer
+    ans_container=soup.find(attrs={'id': 'answers'})
+    selected_ans_container=ans_container.find(attrs={'class': 'answer js-answer accepted-answer'})
+    selected_ans_content=selected_ans_container.find(attrs={'class' : 's-prose js-post-body'})
+    answer_text=selected_ans_content.text
 
-    answer_field=answer_parent1.find_all(attrs={'class' : 's-prose js-post-body'})
-    answer_text=answer_field[0].text
     
     
     # print(ques_text)
     # print(soup.text)
-    return ques, ques_text, answer_text
+    return ques, ques_content, answer_text
 
 response=requests.get(url)
 if response.status_code==200:
@@ -34,6 +38,7 @@ else:
     print(response.status_code)
     
 
+# For Debugging
 print('================process done========================')
 print('-----Question-----------')
 print(question)
