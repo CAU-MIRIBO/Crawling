020822272d28b60e07bd7ab6f2b84bd69cb77a17
hjso <hjsong9th@gmail.com>
2022-03-27 21:10:40 +0900

    combine with crawling1.py

--- html_crawling/general_website_html.py
+++ html_crawling/general_website_html.py
@@ -3,27 +3,45 @@ from tkinter import EW
 from urllib import request
 import requests
 from bs4 import BeautifulSoup
+from newspaper import Article
 
 # from gensim.summarization.summarizer import summarize
 # from newspaper import Article
 
-def general_process(soup):
+def crawling_article(soup):
+    article=Article(url)
+    article.download()
+
+    print(article.html)
+    article.parse()
+    title=article.title
+    contents=article.text
     
-    g_head=soup.head
-    g_title=g_head.find(attrs={'property':'og:title'})
-    g_container=soup.body
-    g_content=g_container #.find(attrs={'class':'e-content post-content'})
-    contents=g_content.text
+    # print(article.text)
+    if article.keywords:
+        a_keywords=article.keywords
+        
+    if article.summary:
+        a_summary=article.summary
+        
     
-    return g_title, contents
+    return title, contents
+
+def header_process(soup):
+    # g_title=g_head.find(attrs={'property':'og:title'})
+    # g_container=soup.body
+    # g_content=g_container #.find(attrs={'class':'e-content post-content'})
+    # contents=g_content.text
+    
+    print('not completed')
 
 def request_through_url(url):
     response=requests.get(url)
     if response.status_code==200:
         html=response.text
         soup=BeautifulSoup(html, 'html.parser')
-        title, contents=general_process(soup)
-        print_test_result(title,contents)
+        title, contents=crawling_article(soup)
+        # print_test_result(title,contents)
         return title, contents
         
     else:
@@ -39,5 +57,10 @@ def print_test_result(title, contents):
     print(contents)
     print('\n')
 
-url='https://leejh0624.tistory.com/316'
-ques, answer=request_through_url(url)
+
+
+url='https://www.hani.co.kr/arti/culture/culture_general/1023318.html'
+# 'https://www.ajunews.com/view/20211215155407703'
+    
+request_through_url(url)
+print('process done')
