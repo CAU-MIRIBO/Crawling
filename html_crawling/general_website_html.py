acc53f54548ee23e982ddb50c04067c36ccb88f4
hjso <hjsong9th@gmail.com>
2022-03-27 23:14:53 +0900

    check javascript/css usage in html

--- html_crawling/general_website_html.py
+++ html_crawling/general_website_html.py
@@ -4,6 +4,17 @@ from bs4 import BeautifulSoup
 from newspaper import Article
 #import nltk
 
+import selenium
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+
 # from gensim.summarization.summarizer import summarize
 # from newspaper import Article
 
@@ -16,7 +27,12 @@ def crawling_article(soup):
     # print(article.html)
     article.parse()
     title=article.title
-    contents=article.text
+    if article.text:
+        contents=article.text
+    else:
+        contents=''
+        print('no contents')
+        get_contents(soup)
     
     # print(article.text)
     # article.nlp()
@@ -33,6 +49,10 @@ def article_nlp(art):
     art.nlp()
     print()
     
+def get_contents(soup):
+    body=soup.body
+    contents_text=body.text
+    print(contents_text)
     
 def header_process(soup):
     # g_title=g_head.find(attrs={'property':'og:title'})
@@ -55,6 +75,14 @@ def request_through_url(url):
         print(response.status_code)
         
         
+def request_with_selenium(Url):
+    driver = webdriver.Chrome(executable_path='chromedriver')
+    driver.get(url=Url)
+    print(driver.current_url)
+    driver.close()
+    
+    driver.implicitly_wait(time_to_wait=5)
+        
 def print_test_result(title, contents):
     
     print('================process done========================')
@@ -66,8 +94,8 @@ def print_test_result(title, contents):
 
 
 
-url='https://www.hani.co.kr/arti/culture/culture_general/1023318.html'
-# 'https://www.ajunews.com/view/20211215155407703'
-    
+url='https://www.ajunews.com/view/20211215155407703'
+# 
+# 'https://www.hani.co.kr/arti/culture/culture_general/1023318.html'
 title, contents=request_through_url(url)
 print_test_result(title,contents)
