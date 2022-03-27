7dbdb7110945b1f9c3c683a51b9c259df9c41152
hjso <hjsong9th@gmail.com>
2022-03-27 21:07:23 +0900

    general_case crawling with newspaper module

--- html_crawling/crawling1.py
+++ html_crawling/crawling1.py
@@ -1,18 +1,54 @@
 from urllib import request
 import requests
 from bs4 import BeautifulSoup
+from newspaper import Article
 
-url='https://www.nbcnews.com/news/us-news/jamal-edwards-music-entrepreneur-helped-launch-ed-sheeran-jessie-j-die-rcna17041'
 
-response=requests.get(url)
+def crawling_article(soup):
+    article=Article(url)
+    article.download()
 
-if response.status_code==200:
-    html=response.text
-    soup=BeautifulSoup(html, 'html.parser')
-    print(soup)
+    print(article.html)
+    article.parse()
+    title=article.title
+    contents=article.text
     
-else:
-    print(response.status_code)
+    # print(article.text)
+    if article.keywords:
+        a_keywords=article.keywords
+        
+    if article.summary:
+        a_summary=article.summary
     
+    
+    return title, contents
 
+def request_through_url(url):
+    response=requests.get(url)
+    if response.status_code==200:
+        html=response.text
+        soup=BeautifulSoup(html, 'html.parser')
+        title, contents=crawling_article(soup)
+        # print_test_result(title,contents)
+        return title, contents
+        
+    else:
+        print(response.status_code)
+        
+        
+def print_test_result(title, contents):
+    
+    print('================process done========================')
+    print('\n----------Question Header-----------\n')
+    print(title)
+    print('\n-------------Answer---------------\n')
+    print(contents)
+    print('\n')
+
+
+
+url='https://www.hani.co.kr/arti/culture/culture_general/1023318.html'
+# 'https://www.ajunews.com/view/20211215155407703'
+    
+request_through_url(url)
 print('process done')
