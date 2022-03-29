03b403b446493b29385dce8804f738901b4e5de4
hjso <hjsong9th@gmail.com>
2022-03-30 03:39:01 +0900

    add selenium module

--- html_crawling/general_website_html.py
+++ html_crawling/general_website_html.py
@@ -15,6 +15,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
+# # 처음 시작할때 Webdriver 관련 오류 해결방법
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+
 # from gensim.summarization.summarizer import summarize
 # from newspaper import Article
 
@@ -76,12 +81,29 @@ def request_through_url(url):
         
         
 def request_with_selenium(Url):
-    driver = webdriver.Chrome(executable_path='chromedriver')
-    driver.get(url=Url)
+    
+    options = webdriver.ChromeOptions()
+    
+    # # 창 사이즈
+    # options.add_argument('window-size=1920,1080')  
+    
+    # 창 띄우지 않기
+    options.add_argument('headless')
+     
+    # Apply options
+    driver = webdriver.Chrome(executable_path='C:\\Users\\hjson\\Downloads\\chromedriver.exe')
+    # driver.implicitly_wait(3)
+    driver.get(Url)
     print(driver.current_url)
+    
+    # 페이지 내에서 값 읽어오기
+    body=driver.find_element_by_tag_name('body')
+    body_text=body.text
+    print(body_text)
+    
     driver.close()
     
-    driver.implicitly_wait(time_to_wait=5)
+    # driver.implicitly_wait(time_to_wait=5)
         
 def print_test_result(title, contents):
     
@@ -95,7 +117,10 @@ def print_test_result(title, contents):
 
 
 url='https://www.ajunews.com/view/20211215155407703'
-# 
 # 'https://www.hani.co.kr/arti/culture/culture_general/1023318.html'
-title, contents=request_through_url(url)
-print_test_result(title,contents)
+
+
+# title, contents=request_through_url(url)
+# print_test_result(title,contents)
+
+request_with_selenium(url)
