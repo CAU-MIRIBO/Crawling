ed9c9d4832bcb35361f5ac78d6208f1ffbf5ab23
hjso <hjsong9th@gmail.com>
2022-03-30 22:52:28 +0900

    get text by paragraph but not entirely

--- html_crawling/quora_html.py
+++ html_crawling/quora_html.py
@@ -8,6 +8,10 @@ from selenium_crawling import *
 
 def quora_process(url):
     
+    quo_ques=0
+    quo_ques_cont=0
+    quo_answer=[]
+    
     # q_head=soup.head
     # q_title=q_head.find(attrs={'property':'og:title'})
     # q_type=q_head.find(attrs={'property':"og:type"})
@@ -24,26 +28,39 @@ def quora_process(url):
     # answer=0
     body, driver=request_with_selenium(url)
     root_container=body.find_element_by_id('root')
-    
+
     # q_title=root_container.find_element_by_xpath('//*[@id="mainContent"]/div[1]/div/div[1]/span/span/div/div/div')
     # q_title=root_container.find_element_by_xpath('//body[contains(@class, question_title)]')
     q_title=root_container.find_element_by_xpath('//*[@id="mainContent"]/div[1]/div')
+    q_answer_box=body.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[3]/div/div/div/div/div/div[1]')
+    q_answer_list=q_answer_box.find_elements_by_tag_name('p')
     
+    for answ in q_answer_list:
+        quo_answer.append(answ.text)
+        print(answ.get_attribute('innerHTML'))
+        print(answ.text)
     
-    # HTML 표시
-    html=root_container.get_attribute('innerHTML')
-    print(html)
-    print('====================')
-    print(q_title.get_attribute('innerHTML'))
+    # for debugging
+    print_paragraph(quo_answer)
+    # html=q_answer.get_attribute('innerHTML')
     
-    text=q_title.text
-    # print(text)
+    # //*[@id="mainContent"]/div[2]/div[3]/div/div/div/div/div/div[1]
+    # //*[@id="mainContent"]/div[2]/div[3]/div/div/div/div/div/div[1]/div/div/div[1]/div[2]/div/div[1]
+    # //*[@id="mainContent"]/div[2]/div[3]/div/div/div/div/div/div[1]/div/div/div[1]/div[2]/div
+    # //*[@id="mainContent"]/div[2]/div[3]/div/div/div/div/div/div[1]/div/div/div[1]/div[2]/div
+    # # HTML 표시
+    # //*[@id="mainContent"]/div[2]/div[3]/div/div/div/div/div/div[1]/div/div/div[1]/div[2]/div/div/div/span/p[1]
+    # //*[@id="mainContent"]/div[2]/div[3]/div/div/div/div/div/div[1]/div/div/div[1]/div[2]/div/div/div/span/p[2]
+   # //*[@id="mainContent"]/div[2]/div[3]/div/div/div/div/div/div[1]/div/div/div[1]/div[2]/div/div/div/span/p[1]
+   
+    # )print('====================')
+    # print(q_title.get_attribute('innerHTML'))
+    # print('====================')
     
-    ques=0
-    answer=0
+    quo_ques=q_title.text
     
     driver.close()
-    return ques, answer
+    return quo_ques, quo_ques_cont, quo_answer
 
 def request_through_url(url):
     response=requests.get(url)
@@ -66,7 +83,11 @@ def print_test_result(question, answer):
     print('\n-------------Answer---------------\n')
     print(answer)
     print('\n')
+    
+def print_paragraph(answ):
+    for a in answ:
+        print(a)
 
 url='https://www.quora.com/How-is-the-culture-of-Jeju-Island-different-from-the-rest-of-South-Korea'
 # ques, answer=request_through_url(url)
-a,b=quora_process(url)
+a,b,c=quora_process(url)
