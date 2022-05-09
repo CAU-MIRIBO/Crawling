e24db66c8119c39d4684cd5b80d4bf46cf9d2121
wonawonana <56299516+wonawonana@users.noreply.github.com>
2022-05-09 23:51:49 +0900

    flask test with Ajax option 124

--- html_crawling/flask_app.py
+++ html_crawling/flask_app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request as RRR
 from flask_restx import Api, Resource
+from flask_cors import CORS
 #import urllib
 from html_crawling.get_url_data import get_url_data
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 # @app.route("/start",methods=["POST",'GET'])
 # def start():
@@ -23,6 +25,17 @@ def option2():
     #print(x.option(2))
     return str(x.option(2))
 
+
+@app.route('/ajax', methods=['POST'])
+def ajax():
+    data = RRR.get_json()
+    print(data)
+    x = get_url_data()
+    url=data['url']
+    x.text_for_one_url(str(url))
+    res2=x.option(4)
+    print(res2)
+    return jsonify(result = "success", result2= res2)
 
 
 if __name__ == '__main__':
